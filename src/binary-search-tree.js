2f16232c4a7cda0641851036a43dde1dfc2f6b6f
SergeiOsmolovskii <osmolovskii_sergei@mail.ru>
2021-10-15 15:32:58 +0300

    feat: add remove method for binary-search tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,42 +1,48 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
-const { Node } = require('../extensions/list-tree.js');
+const {
+  Node
+} = require('../extensions/list-tree.js');
 
 /**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
+ * Implement simple binary search tree according to task description
+ * using Node from extensions
+ */
 module.exports = class BinarySearchTree {
+
   constructor() {
-    this.root = null;
+    this.rootNode = null;
   }
 
   root() {
-    return this.root;
+    return this.rootNode;
   }
 
   add(data) {
-  
+
     const addInside = (node, data) => {
-      if(!node) {
+      if (!node) {
         return new Node(data);
       }
 
       if (node.data === data) {
         return node;
       }
-      
+
       if (data < node.data) {
         node.left = addInside(node.left, data);
       } else {
         node.right = addInside(node.right, data);
       }
       return node;
-    } 
-    this.root = addInside(this.root, data)
+    }
+    this.rootNode = addInside(this.rootNode, data);
   }
 
   has(data) {
+
     const hasInside = (node, data) => {
       if (!node) {
         return false;
@@ -52,55 +58,85 @@ module.exports = class BinarySearchTree {
         return hasInside(node.right, data);
       }
     }
-    return hasInside(this.root, data);
+    return hasInside(this.rootNode, data);
   }
-
-  
 
   find(data) {
 
     const findInside = (node, data) => {
-      
-    if (!node) {
-      return null;
-    }
 
-    if (node.data === data) {
-      return node;
-    }  
+      if (!node) {
+        return null;
+      }
 
-    if (data < node.data) {
-      return findInside(node.left, data);
-    } else {
-      return findInside(node.right, data);
+      if (node.data === data) {
+        return node;
+      }
+
+      if (data < node.data) {
+        return findInside(node.left, data);
+      } else {
+        return findInside(node.right, data);
+      }
     }
+    return findInside(this.rootNode, data);
   }
-  return findInside(this.root, data);
-  }
-  
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  remove(data) {
+
+    const removeInside = (node, data) => {
+      if (!node) {
+        return null;
+      }
+
+      if (data < node.data) {
+        node.left = removeInside(node.left, data);
+        return node;
+      } else if (data > node.data) {
+        node.right = removeInside(node.right, data);
+        return node;
+      } else {
+        if (!node.left && !node.right) {
+          return null;
+        }
+
+        if (!node.left) {
+          node = node.right;
+          return node
+        }
+
+        if (!node.right) {
+          node = node.left;
+          return node;
+        }
+
+        let minFromRight = node.right;
+        while (minFromRight.left) {
+          minFromRight = minFromRight.left;
+        }
+        node.data = minFromRight.data;
+        node.right = removeInside(node.right, minFromRight.data);
+        return node;
+      }
+    }
+    return removeInside(this.rootNode, data);
   }
 
   min() {
-    if (!this.root) return;
-    let node = this.root;
-    while(node.left) {
+    if (!this.rootNode) return;
+    let node = this.rootNode;
+    while (node.left) {
       node = node.left;
     }
     return node.data;
   }
 
   max() {
-    if (!this.root) return;
-    
-    let node = this.root;
-
-    while(node.right) {
+    if (!this.rootNode) return;
+    let node = this.rootNode;
+    while (node.right) {
       node = node.right;
     }
     return node.data;
   }
-
 }
